6e61dd144b0825e2d8a6842988adc290c788660d
chao-ji <jic@indiana.edu>
2017-05-14 13:54:45 -0400

    Add files via upload

--- collaborative_filter.py
+++ collaborative_filter.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-NUM_USER = 6040
-NUM_ITEM = 3952
-
 class MiniBatchFeeder(object):
     def __init__(self,
                  data,
@@ -61,11 +58,11 @@ class CollaborativeFilter(object):
         self.learning_rate = learning_rate
         self.reg = reg
 
-        self.bias_, self.bias_user_, self.bias_item_, self.embd_user_, self.embd_item_ = self._create_param_tensors()
-        self.embds_ = None
+        self.embds_ = dict() 
+        self.log_rmse_ = dict()
 
     def _create_param_tensors(self):
-        with tf.device("/cpu:0") :
+        with tf.device("/cpu:0"):
             bias = tf.get_variable(name="bias", shape=[], initializer=self.weight_initializer)
             bias_user = tf.get_variable(name="bias_user", shape=[self.num_user], initializer=self.weight_initializer)
             bias_item = tf.get_variable(name="bias_item", shape=[self.num_item], initializer=self.weight_initializer)
@@ -75,9 +72,9 @@ class CollaborativeFilter(object):
         return bias, bias_user, bias_item, embd_user, embd_item
 
 
-    def _create_op_tensors(self, user_batch_index, item_batch_index, rating_batch):
+    def _create_op_tensors(self, user_batch_index, item_batch_index, rating_batch, bias, bias_user, bias_item, embd_user, embd_item):
 
-        rating_pred, (bias_user_batch, bias_item_batch, embd_user_batch, embd_item_batch) = self._create_pred_tensor(user_batch_index, item_batch_index)
+        rating_pred, (bias_user_batch, bias_item_batch, embd_user_batch, embd_item_batch) = self._create_pred_tensor(user_batch_index, item_batch_index, bias, bias_user, bias_item, embd_user, embd_item)
 
         global_step = tf.train.get_global_step()
         assert global_step is not None
@@ -97,16 +94,16 @@ class CollaborativeFilter(object):
 
         return train_op
             
-    def _create_pred_tensor(self, user_batch_index, item_batch_index):
+    def _create_pred_tensor(self, user_batch_index, item_batch_index, bias, bias_user, bias_item, embd_user, embd_item):
         with tf.device(self.device):
-            bias_user_batch = tf.nn.embedding_lookup(params=self.bias_user_, ids=user_batch_index, name="bias_user_batch")
-            bias_item_batch = tf.nn.embedding_lookup(params=self.bias_item_, ids=item_batch_index, name="bias_item_batch")
+            bias_user_batch = tf.nn.embedding_lookup(params=bias_user, ids=user_batch_index, name="bias_user_batch")
+            bias_item_batch = tf.nn.embedding_lookup(params=bias_item, ids=item_batch_index, name="bias_item_batch")
 
-            embd_user_batch = tf.nn.embedding_lookup(params=self.embd_user_, ids=user_batch_index, name="embd_user_batch")
-            embd_item_batch = tf.nn.embedding_lookup(params=self.embd_item_, ids=item_batch_index, name="embd_item_batch")
+            embd_user_batch = tf.nn.embedding_lookup(params=embd_user, ids=user_batch_index, name="embd_user_batch")
+            embd_item_batch = tf.nn.embedding_lookup(params=embd_item, ids=item_batch_index, name="embd_item_batch")
 
             rating_pred = tf.reduce_sum(tf.multiply(embd_user_batch, embd_item_batch), 1)
-            rating_pred = tf.add(rating_pred, self.bias_)
+            rating_pred = tf.add(rating_pred, bias)
             rating_pred = tf.add(rating_pred, bias_user_batch)
             rating_pred = tf.add(rating_pred, bias_item_batch, name="rating_pred") 
 
@@ -114,21 +111,26 @@ class CollaborativeFilter(object):
 
     def fit(self, train_data, test_data=None):
 
+        bias, bias_user, bias_item, embd_user, embd_item = self._create_param_tensors()
+
         user_batch_index = tf.placeholder(tf.int32, name="user_batch_index")
         item_batch_index = tf.placeholder(tf.int32, name="item_batch_index")
         rating_batch = tf.placeholder(tf.float32, name="rating_batch")
         
         global_step = tf.contrib.framework.get_or_create_global_step()
 
-        rating_pred, _ = self._create_pred_tensor(user_batch_index, item_batch_index)
-        train_op = self._create_op_tensors(user_batch_index, item_batch_index, rating_batch)
+
+        rating_pred, _ = self._create_pred_tensor(user_batch_index, item_batch_index, bias, bias_user, bias_item, embd_user, embd_item)
+        train_op = self._create_op_tensors(user_batch_index, item_batch_index, rating_batch, bias, bias_user, bias_item, embd_user, embd_item)
 
         init_op = tf.global_variables_initializer()
+
+        train_rmse, test_rmse = [], []
 
         with tf.Session() as sess:
             sess.run(init_op)
             for i in range(self.num_epoch):
-                print i
+#                print i
 
                 train_data.reset_counter()
                 for batch in train_data:
@@ -137,12 +139,20 @@ class CollaborativeFilter(object):
                                                                  rating_batch: batch[:, 2]})
 
                 train_pred, train_true = self._eval(sess, rating_pred, train_data, user_batch_index, item_batch_index, with_target=True)
-                test_pred, test_true = self._eval(sess, rating_pred, test_data, user_batch_index, item_batch_index, with_target=True)
+                train_rmse.append(np.sqrt(np.mean(np.square(train_pred - train_true))))
 
-                print "%f\t%f" % (np.sqrt(np.mean(np.square(train_pred - train_true))), np.sqrt(np.mean(np.square(test_pred - test_true))))
+                if test_data is not None:
+                    test_pred, test_true = self._eval(sess, rating_pred, test_data, user_batch_index, item_batch_index, with_target=True)
+                    test_rmse.append(np.sqrt(np.mean(np.square(test_pred - test_true))))
+
+            self.log_rmse_["train_rmse"] = np.array(train_rmse)
+            if test_data is not None:
+                self.log_rmse_["test_rmse"] = np.array(test_rmse)
 
             self.embds_ = dict(zip(["bias", "bias_user", "bias_item", "embd_user", "embd_item"],
-                               sess.run([self.bias_, self.bias_user_, self.bias_item_, self.embd_user_, self.embd_item_])))
+                               sess.run([bias, bias_user, bias_item, embd_user, embd_item])))
+
+        tf.reset_default_graph() 
 
     def predict(self, test_data):
         pred = []
@@ -166,7 +176,7 @@ class CollaborativeFilter(object):
 
         pred = np.clip(np.array(pred), 1.0, 5.0)
         true = np.array(true)
-        print "%f" % np.sqrt(np.mean(np.square(pred - true)))
+#        print "%f" % np.sqrt(np.mean(np.square(pred - true)))
 
         return pred, true
 
